516530f0b2e49846531b99f8319c579c2d6b5396
unsync <unsync@example.com>
2024-01-24 18:14:56 +0100

    add import order rule

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ import antfu from '@antfu/eslint-config'
 const defaults = {
   rules: {
     'style/brace-style': ['error', '1tbs'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin', // Built-in imports (come from NodeJS native) go first
+          'external', // <- External imports
+          'internal', // <- Absolute imports
+          ['sibling', 'parent'], // <- Relative imports, the sibling and parent types they can be mingled together
+          'index', // <- index imports
+          'unknown', // <- unknown
+        ],
+        alphabetize: {
+          /* sort in ascending order. Options: ["ignore", "asc", "desc"] */
+          order: 'asc',
+          /* ignore case. Options: [true, false] */
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
 }
 
