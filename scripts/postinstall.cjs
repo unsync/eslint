585236e80df3bf2f52ee0601ce499e477d9249c9
unsync <unsync@example.com>
2024-01-28 15:48:14 +0100

    postintall

---
+++ scripts/postinstall.cjs
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+const rootPath = require.main.paths[0].split('node_modules')[0]
+
+// add npm script
+const packageJsonPath = `${rootPath}package.json`
+if(fs.existsSync(packageJsonPath)) {
+    console.info('postinstall: add package.json scripts')
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath))
+
+    packageJson.scripts['lint'] = 'eslint .'
+    packageJson.scripts['lint:fix'] = 'eslint . --fix'
+
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
+}
+
+const eslintConfigPath = `${rootPath}eslint.config.js`
+if(fs.existsSync(eslintConfigPath)) {
+    const content = '// eslint.config.js\n' +
+        'import eslint from \'./index.js\'\n' +
+        '\n' +
+        'export default eslint()\n'
+    console.info('postinstall: add eslint.config.js')
+    fs.writeFileSync(eslintConfigPath, content);
+}
